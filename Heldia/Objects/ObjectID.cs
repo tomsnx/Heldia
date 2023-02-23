8811d42600de2e5b29945aa3478b8b6e924a4075
Tom SIOUAN <102312082+tomsnx@users.noreply.github.com>
2023-02-23 21:05:53 +0100

    Delete Heldia directory

--- Heldia/Objects/ObjectID.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Heldia
-{
-    public static class ObjectID
-    {
-        public const int player = 0;
-        public const int block = 1;
-        public const int tree = 2;
-    }
-}
